6ab34e9070c1e84aa9f94df72d267799d92ce7b2
Samarth Rangavittal <szr165@brubeck.bx.psu.edu>
2017-06-07 14:40:53 -0400

    experimented with different datasets but reverted to small dataset due to github file size limit

--- dependency/run_dsk.sh
+++ dependency/run_dsk.sh
@@ -12,21 +12,16 @@ fi
 
 R1_fsY_reads=$1
 
-echo $R1_fsY_reads
+echo "Counting k-mers from the following file : " $R1_fsY_reads
 
-time dsk-v2.2.0-bin-Linux/bin/dsk -file $R1_fsY_reads -abundance-min 0 -kmer-size 25 -out R1_dsk -verbose 0
+dsk-v2.2.0-bin-Linux/bin/dsk -file $R1_fsY_reads -abundance-min 0 -kmer-size 25 -out R1_dsk -verbose 0
 
-time dsk-v2.2.0-bin-Linux/bin/h5dump -y -d dsk/histogram R1_dsk.h5 | grep "^\ *[0-9]" | tr -d " " | tr -d "," | paste - - > R1_dsk.histo
+dsk-v2.2.0-bin-Linux/bin/dsk2ascii -file R1_dsk -out kmer_counts_from_dsk -verbose 0
 
-time dsk-v2.2.0-bin-Linux/bin/dsk2ascii -file R1_dsk -out kmer_counts_from_dsk -verbose 0
-
+rm R1_dsk.h5
 
 rm -rf dsk_output
 mkdir dsk_output
-mv R1* dsk_output/
+mv kmer_counts_from_dsk dsk_output/
 
-rm -rf tables
-mkdir tables
-mv pre_Threshold_reference_table tables/
-
-
+echo "Counting finished! K-mer counts are in the folder './dsk_output'"
